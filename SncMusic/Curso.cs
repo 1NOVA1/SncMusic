f7e086335963637b335352ea4596385a6006f7ce
Gui Araujo <santosguilherme2019@outlook.com>
2020-01-23 17:53:23 -0300

    Quarto commit

--- SncMusic/Curso.cs
+++ SncMusic/Curso.cs
@@ -59,44 +59,123 @@ namespace SncMusic
                 comm.Parameters.Add("@carga_horaria", MySqlDbType.Int32).Value = cargaHoraria;
                 comm.Parameters.Add("@valor", MySqlDbType.Decimal).Value = valor;
                 comm.ExecuteNonQuery();
-                comm.CommandText = "selec @@identity";
+                comm.CommandText = "select @@identity";
                 id = Convert.ToInt32(comm.ExecuteScalar());
                 Banco.Fechar();
 
             }
             catch (Exception)
             {
-
                 throw;
             }
            
         }
-        public bool Alterar()
+        public bool Alterar(Curso curso)
         {
-            return true;
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "update tb_curso set nome_curso = @nome," +
+                    " carga_horaria_curso = @carga" +
+                    ", valor_curso = @valor where id_curso = @id";
+                comm.Parameters.Add("id", MySqlDbType.Int32).Value = curso.id;
+                comm.Parameters.Add("@nome", MySqlDbType.VarChar).Value = curso.nome;
+                comm.Parameters.Add("@carga", MySqlDbType.Int32).Value = curso.cargaHoraria;
+                comm.Parameters.Add("@valor", MySqlDbType.Decimal).Value = curso.valor;
+                comm.ExecuteNonQuery();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
-        public void ConsultarPorId(int id)
+        public void ConsultarPorId(int _id)
         {
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = ("select * from tb_curso where id = @id =") + _id;
+                var dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    Id = dr.GetInt32(0);
+                    Nome = dr.GetString(1);
+                    CargaHoraria = dr.GetInt32(2);
+                    Valor = Convert.ToDouble(dr.GetDecimal(3));
+                }
+
+            }
+
+
+            catch (Exception)
+            {
+
+
+            }
 
         }
         public List <Curso> ConsultarPorNome(string caracteres) 
         {
+
             List<Curso> lista = new List<Curso>();
-            return lista;
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "select * from tb_curso where nome_curso like '%@carac%'";
+                comm.Parameters.Add("@carac", MySqlDbType.VarChar).Value = caracteres;
+                var dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new Curso(dr.GetInt32(0), dr.GetString(1), dr.GetInt32(2), dr.GetDouble(3)));
+
+                }
+                return lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
 
 
         }
         public bool AssociarProfessor(int _id_curso, int _id_professor)
         {
-            return true;
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = ("insert into professor_curso values(@prof, @curso, default)");
+                comm.Parameters.Add("@prof", MySqlDbType.Int32).Value = _id_professor;
+                comm.Parameters.Add("@curso", MySqlDbType.Int32).Value = _id_curso;
+                comm.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {  
+                return true;
+            }
+            
         }
         public List<Professor> ConsultarAssociacao(int _id_curso)
         {
             List<Professor> lista = new List<Professor>();
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "select * from tb_professor_curso";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return lista;
             
         }
+
+
 
 
 
